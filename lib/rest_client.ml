25bca0d0b0e35a301cf43d8c5235453df0e22e34
Akhil Kandi <akhilprm999@gmail.com>
2025-04-21 01:10:50 +0400

    WIP: Add rest_client.ml

---
+++ lib/rest_client.ml
@@ -0,0 +1,25 @@
+(* src/rest_client.ml *)
+
+open Lwt.Infix
+open Cohttp_lwt_unix
+
+let base_url = "http://182.76.70.89:3001"
+
+let login ~username ~password =
+  let uri = Uri.of_string (base_url ^ "/auth/greek/sessiontoken") in
+  let headers = Cohttp.Header.init_with "Content-Type" "application/json" in
+  let body_json = `Assoc [
+    ("user_id", `String username);
+    ("password", `String password)
+  ] in
+  let body = Cohttp_lwt.Body.of_string (Yojson.Basic.to_string body_json) in
+
+  Client.post ~headers ~body uri
+  >>= fun (resp, body_stream) ->
+  Cohttp_lwt.Body.to_string body_stream
+  >|= fun body_str ->
+  let json = Yojson.Basic.from_string body_str in
+  let open Yojson.Basic.Util in
+  let session_token = json |> member "session_token" |> to_string in
+  let user_id = json |> member "user_id" |> to_string in
+  Config.with_session Config.empty ~session_token ~user_id
