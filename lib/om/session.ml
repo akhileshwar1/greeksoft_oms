0f8e302cce2b3b4ac09690431f2b93e5f17d1412
Akhil Kandi <akhilprm999@gmail.com>
2025-04-25 12:19:51 +0400

    Add config passing comment.

--- lib/om/session.ml
+++ lib/om/session.ml
@@ -5,7 +5,7 @@ let login ~username ~password =
   match empty.broker with
   | "greeksoft" ->
       Greeksoft.Rest_client.login ~username ~password
-      >>= Greeksoft.Rest_client.get_flag_values
+      >>= Greeksoft.Rest_client.get_flag_values (* Rare case of passing configs to greeksoft layer *)
       >>= Greeksoft.Rest_client.get_login_info
       >>= Greeksoft.Rest_client.jlogin_new
   | _ ->
